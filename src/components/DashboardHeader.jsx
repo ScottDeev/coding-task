046d8e3117e1e9df5ef214545fc9cf760fc26219
ScottDeev <amehelijah99@gmail.com>
2022-11-04 21:02:09 +0100

    implemented the logout feature

--- src/components/DashboardHeader.jsx
+++ src/components/DashboardHeader.jsx
@@ -1,9 +1,13 @@
+import React from 'react'
+import { AuthContext } from '../authContext';
 import logoutIcon from '../assests/image/logout.svg'
+
 export default function DashboardHeader() {
+  const { dispatch } = React.useContext(AuthContext);
   return (
     <header className='flex flex-row w-full justify-between text-white items-center'>
       <h1 className='text-[48px] font-[900]'>APP</h1>
-      <button className='bg-[#9BFF00] rounded-[40px] flex py-[12px] px-[24px]'>
+      <button className='bg-[#9BFF00] rounded-[40px] flex py-[12px] px-[24px]' onClick={() => dispatch({type: 'LOGOUT'})}>
         <img src={logoutIcon} alt="" />
         <span className='text-black'>Logout</span>
       </button>
